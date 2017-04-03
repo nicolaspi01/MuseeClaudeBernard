6779a8e39df8a22f4cca7cc622c558ca3ca1537c
OktayAgacseven <yatko@hotmail.fr>
2017-04-03 20:51:03 +0200

    Uniquement suppression en tant qu'admin

--- Views/oeuvres.php
+++ Views/oeuvres.php
@@ -12,8 +12,10 @@
 				echo '<tr>
 				<td><img class="oeuvres" src="Web/img/'.str_replace(' ', '', $ligne['Intitule']).'.jpg"</td>
 				<td>'.$ligne['Intitule'].'</td>
-				<td>'.$ligne['Auteur'].'</td>
-				<td><a href="index.php?action=Supp&id='.$ligne['idOeuvre'].'">Supprimer</a>
+				<td>'.$ligne['Auteur'].'</td>';
+				
+				if ( isset($_SESSION['Admin']))	if(  ($_SESSION['Admin']==true) )
+						echo '<td><a href="index.php?action=Supp&id='.$ligne['idOeuvre'].'">Supprimer</a>
 				</tr>';
 	}
 	echo '</table>';
